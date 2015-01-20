87b7a3b3bad6969749dd58d2a4700a16c5f080c3
André Walker <andre@andrewalker.net>
2015-01-19 23:23:08 -0200

    Create Redirect controller

---
+++ lib/Porto6/Web/Controller/Redirect.pm
@@ -0,0 +1,65 @@
+package Porto6::Web::Controller::Redirect;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+sub redirect :Path('/redirect') :Args(1) {
+    my ( $self, $ctx, $payment_id ) = @_;
+
+    my $sale = $ctx->model('DB::Sale')->find($payment_id);
+
+    if (!$sale) {
+        die 'Pagamento não encontrado';
+    }
+
+    if ($sale->status ne 'new') {
+        die 'Status é ' . $sale->status;
+    }
+
+    my $cpi = $ctx->model('CPI')->get($sale->gateway);
+    my $client = $sale->client;
+
+    my $cart = $cpi->new_cart({
+        buyer => {
+            name => $client->name,
+            email => $client->email,
+        }
+    });
+
+    $cart->add_item({
+        id       => 'seis-meses-no-porto',
+        quantity => $sale->items->count,
+        price    => 5.00,
+        description => 'Cupons para sorteio dos quadros',
+    });
+
+    $ctx->res->body($cart->get_form_to_pay($sale->id));
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=encoding utf-8
+
+=head1 NAME
+
+Porto6::Web::Controller::Redirect - Redirect Controller for Porto6::Web
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 redirect
+
+=head1 AUTHOR
+
+André Walker
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
