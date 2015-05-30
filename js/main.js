4708e72d84ffe068aea4645df20fba55e811866f
André Walker <andre@andrewalker.net>
2015-05-30 15:38:18 -0300

    Remove weirdness of main.js

--- js/main.js
+++ js/main.js
@@ -49,81 +49,81 @@ BRUSHED.listenerMenu = function(){
 BRUSHED.slider = function(){
     $.supersized({
         // Functionality
-        slideshow               :   1,            // Slideshow on/off
-        autoplay                :    1,            // Slideshow starts playing automatically
-        start_slide             :   1,            // Start slide (0 is random)
-        stop_loop                :    0,            // Pauses slideshow on last slide
-        random                    :     0,            // Randomize slide order (Ignores start slide)
-        slide_interval          :   12000,        // Length between transitions
-        transition              :   1,             // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-        transition_speed        :    300,        // Speed of transition
-        new_window                :    1,            // Image links open in new window/tab
-        pause_hover             :   0,            // Pause slideshow on hover
-        keyboard_nav            :   1,            // Keyboard navigation on/off
-        performance                :    1,            // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-        image_protect            :    1,            // Disables image dragging and right click with Javascript
+        slideshow               :   1,         // Slideshow on/off
+        autoplay                :   1,         // Slideshow starts playing automatically
+        start_slide             :   1,         // Start slide (0 is random)
+        stop_loop               :   0,         // Pauses slideshow on last slide
+        random                  :   0,         // Randomize slide order (Ignores start slide)
+        slide_interval          :   9000,     // Length between transitions
+        transition              :   1,         // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+        transition_speed        :   400,       // Speed of transition
+        new_window              :   1,         // Image links open in new window/tab
+        pause_hover             :   0,         // Pause slideshow on hover
+        keyboard_nav            :   1,         // Keyboard navigation on/off
+        performance             :   1,         // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+        image_protect           :   1,         // Disables image dragging and right click with Javascript
 
         // Size & Position
-        min_width                :   0,            // Min width allowed (in pixels)
-        min_height                :   0,            // Min height allowed (in pixels)
-        vertical_center         :   1,            // Vertically center background
-        horizontal_center       :   1,            // Horizontally center background
-        fit_always                :    0,            // Image will never exceed browser width or height (Ignores min. dimensions)
-        fit_portrait             :   1,            // Portrait images will not exceed browser height
-        fit_landscape            :   0,            // Landscape images will not exceed browser width
+        min_width               :   0,         // Min width allowed (in pixels)
+        min_height              :   0,         // Min height allowed (in pixels)
+        vertical_center         :   1,         // Vertically center background
+        horizontal_center       :   1,         // Horizontally center background
+        fit_always              :   0,         // Image will never exceed browser width or height (Ignores min. dimensions)
+        fit_portrait            :   1,         // Portrait images will not exceed browser height
+        fit_landscape           :   0,         // Landscape images will not exceed browser width
 
         // Components
-        slide_links                :    'blank',    // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-        thumb_links                :    0,            // Individual thumb links for each slide
-        thumbnail_navigation    :   0,            // Thumbnail navigation
-        slides                     :      [            // Slideshow Images
+        slide_links             :   'blank',   // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+        thumb_links             :   0,         // Individual thumb links for each slide
+        thumbnail_navigation    :   0,         // Thumbnail navigation
+        slides                  :   [          // Slideshow Images
           {
             image: 'slider/07.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/01.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/02.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/03.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/04.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/05.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           },
           {
             image: 'slider/06.jpg',
-            title: '<div class="slide-content">Seis meses no Porto</div>',
+          //title: '<div class="slide-content">Seis meses no Porto</div>',
             thumb: '',
             url:   ''
           }
         ],
 
         // Theme Options
-        progress_bar            :    0,            // Timer for each slide
-        mouse_scrub                :    0
+        progress_bar : 0, // Timer for each slide
+        mouse_scrub  : 0
 
     });
 
@@ -200,17 +200,19 @@ BRUSHED.fancyBox = function(){
     if($('.fancybox').length > 0 || $('.fancybox-media').length > 0 || $('.fancybox-various').length > 0){
 
         $(".fancybox").fancybox({
-                padding : 0,
+                padding : 10,
+                arrows: false,
                 beforeShow: function () {
                     this.title = $(this.element).attr('title');
                     this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
                 },
                 helpers : {
-                    title : { type: 'inside' },
+                    title : { type: 'over' },
                 }
             });
 
         $('.fancybox-media').fancybox({
+            showNavArrows: false,
             openEffect  : 'none',
             closeEffect : 'none',
             helpers : {
@@ -282,12 +284,12 @@ BRUSHED.contactForm = function(){
             contentType: 'application/json; charset=UTF-8',
             success: function(response) {
                 var payment_id = response.sale.id;
-                if (gateway !== 'Deposit') {
-                    location.href = 'https://payment.seismesesnoporto.ga/redirect/' + payment_id;
+
+                if (gateway === 'Deposit') {
+                    location.href = 'https://seismesesnoporto.ga/deposit.html';
                 }
                 else {
-                    alert("Obrigado! Envie comprovante para pagamento@seismesesnoporto.ga.");
-                    $('#checkout-form')[0].reset();
+                    location.href = 'https://payment.seismesesnoporto.ga/redirect/' + payment_id;
                 }
             },
             error: function () {
